13ba9bfc6b29eafd21065db0d6dcf023527fb2c5
Travis Westura <twestura@gmail.com>
2022-09-18 18:20:49 -0400

    Remove example test.

--- src/add.test.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from "./add.js";
-
-describe("add module", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 /** Main script for running the words game. */
 
-import { sum } from "./add.js";
-
 /** Runs the game. */
 const main: () => void = () => {
   console.log("WORDS!");
-  console.log(sum(1, 2));
 };
 
 main();
