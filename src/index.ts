1202d97e448b1149ede6cc1b42a1a50a5fdd0036
Travis Westura <twestura@gmail.com>
2022-09-18 17:52:09 -0400

    Add initial unit test support.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 /** Main script for running the words game. */
 
+import { sum } from "./add.js";
+
 /** Runs the game. */
 const main: () => void = () => {
   console.log("WORDS!");
+  console.log(sum(1, 2));
 };
 
 main();
